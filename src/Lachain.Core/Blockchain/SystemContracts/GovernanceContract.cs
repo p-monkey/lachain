efff7b9a2cabfb1d5ee1d437ae33fbfdeeb04a04
tbssajal <sajalbuet15@gmail.com>
2022-10-20 21:17:30 +0600

    checking if validator sent keygen confirm tx

--- src/Lachain.Core/Blockchain/SystemContracts/GovernanceContract.cs
+++ src/Lachain.Core/Blockchain/SystemContracts/GovernanceContract.cs
@@ -303,6 +303,20 @@ namespace Lachain.Core.Blockchain.SystemContracts
 
             frame.ReturnValue = new byte[] { };
             frame.UseGas(GasMetering.KeygenConfirmCost);
+
+            var senderPubKey = _context.Receipt.RecoverPublicKey(
+                HardforkHeights.IsHardfork_9Active(_context.Snapshot.Blocks.GetTotalBlockHeight())
+            );
+            var nextValidators = _nextValidators.Get()
+                .Batch(CryptoUtils.PublicKeyLength)
+                .Select(x => x.ToArray().ToPublicKey())
+                .ToArray();
+            if (!nextValidators.Contains(senderPubKey))
+            {
+                Logger.LogDebug($"non validator (public key: {senderPubKey.ToHex()}) sent MethodKeygenConfirm tx");
+                return ExecutionStatus.ExecutionHalted;
+            }
+
             var players = thresholdSignaturePublicKeys.Length;
             var faulty = (players - 1) / 3;
 
@@ -360,6 +374,20 @@ namespace Lachain.Core.Blockchain.SystemContracts
 
             frame.ReturnValue = new byte[] { };
             frame.UseGas(GasMetering.KeygenConfirmCost);
+
+            var senderPubKey = _context.Receipt.RecoverPublicKey(
+                HardforkHeights.IsHardfork_9Active(_context.Snapshot.Blocks.GetTotalBlockHeight())
+            );
+            var nextValidators = _nextValidators.Get()
+                .Batch(CryptoUtils.PublicKeyLength)
+                .Select(x => x.ToArray().ToPublicKey())
+                .ToArray();
+            if (!nextValidators.Contains(senderPubKey))
+            {
+                Logger.LogDebug($"non validator (public key: {senderPubKey.ToHex()}) sent MethodKeygenConfirmWithVerification tx");
+                return ExecutionStatus.ExecutionHalted;
+            }
+
             var players = thresholdSignaturePublicKeys.Length;
             var faulty = (players - 1) / 3;
 
