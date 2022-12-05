541794667368768424f9f30812b9c20f99e0dab7
tbssajal <sajalbuet15@gmail.com>
2022-12-06 01:06:36 +0600

    add unit tests

--- test/Lachain.CoreTest/Blockchain/SystemContracts/StakingContractTest.cs
+++ test/Lachain.CoreTest/Blockchain/SystemContracts/StakingContractTest.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Numerics;
 using System.Reflection;
+using Lachain.Core.Blockchain.Genesis;
+using Lachain.Core.Blockchain.Hardfork;
 using Lachain.Core.Blockchain.Interface;
 using Lachain.Core.Blockchain.SystemContracts;
 using Lachain.Core.Blockchain.SystemContracts.ContractManager;
@@ -15,6 +17,7 @@ using Lachain.Core.DI.Modules;
 using Lachain.Core.DI.SimpleInjector;
 using Lachain.Crypto;
 using Lachain.Crypto.ECDSA;
+using Lachain.Networking;
 using Lachain.Proto;
 using Lachain.Storage.State;
 using Lachain.Utility;
@@ -23,7 +26,6 @@ using Lachain.UtilityTest;
 using NUnit.Framework;
 
 namespace Lachain.CoreTest.Blockchain.SystemContracts
-
 {
     public class StakingContractTest
     {
@@ -32,6 +34,8 @@ namespace Lachain.CoreTest.Blockchain.SystemContracts
         
         private IStateManager _stateManager = null!;
         private IContractRegisterer _contractRegisterer = null!;
+        private IConfigManager _configManager = null!;
+        private IBlockManager _blockManager = null!;
 
         [SetUp]
         public void Setup()
@@ -51,6 +55,17 @@ namespace Lachain.CoreTest.Blockchain.SystemContracts
             
             _stateManager = _container.Resolve<IStateManager>();
             _contractRegisterer = _container.Resolve<IContractRegisterer>();
+            _blockManager = _container.Resolve<IBlockManager>();
+            _configManager = _container.Resolve<IConfigManager>();
+            // set chainId from config
+            if (TransactionUtils.ChainId(false) == 0)
+            {
+                var chainId = _configManager.GetConfig<NetworkConfig>("network")?.ChainId;
+                var newChainId = _configManager.GetConfig<NetworkConfig>("network")?.NewChainId;
+                TransactionUtils.SetChainId((int)chainId!, (int)newChainId!);
+                HardforkHeights.SetHardforkHeights(_configManager.GetConfig<HardforkConfig>("hardfork") ?? throw new InvalidOperationException());
+                StakingContract.Initialize(_configManager.GetConfig<NetworkConfig>("network")!);
+            }
         }
 
         [TearDown]
@@ -63,6 +78,7 @@ namespace Lachain.CoreTest.Blockchain.SystemContracts
         [Test]
         public void Test_OneNodeCycle()
         {
+            _blockManager.TryBuildGenesisBlock();
             var tx = new TransactionReceipt();
 
             var keyPair = new EcdsaKeyPair("0xD95D6DB65F3E2223703C5D8E205D98E3E6B470F067B0F94F6C6BF73D4301CE48"
@@ -75,12 +91,6 @@ namespace Lachain.CoreTest.Blockchain.SystemContracts
 
             var stakeAmount = BigInteger.Pow(10, 21);
 
-            // Set balance for the staker
-            {
-                context.Snapshot.Balances.SetBalance(sender, Money.Parse("1000"));
-                Assert.AreEqual(Money.Parse("1000"),context.Snapshot.Balances.GetBalance(sender));
-            }
-            
             // Become staker
             {
                 var input = ContractEncoder.Encode(StakingInterface.MethodBecomeStaker, publicKey, stakeAmount.ToUInt256());
@@ -107,7 +117,35 @@ namespace Lachain.CoreTest.Blockchain.SystemContracts
                 Assert.IsNotNull(call);
                 var frame = new SystemContractExecutionFrame(call!, context, input, 100_000_000);
                 Assert.AreEqual(ExecutionStatus.Ok, contract.IsAbleToBeValidator(sender, frame));
-                //Assert.AreEqual(1, BitConverter.ToInt32(frame.ReturnValue, 0));
+                Assert.AreEqual(1.ToUInt256(), frame.ReturnValue.ToUInt256());
+            }
+
+            var validatorsInfo = _configManager.GetConfig<GenesisConfig>("genesis")!.Validators;
+            var totalStake = Money.Parse("1000");
+
+            foreach (var info in validatorsInfo)
+            {
+                totalStake += Money.Parse(info.StakeAmount!);
+            }
+
+            // Get total active stake
+            {
+                var input = ContractEncoder.Encode(StakingInterface.MethodGetTotalActiveStake);
+                var call = _contractRegisterer.DecodeContract(context, ContractRegisterer.StakingContract, input);
+                Assert.IsNotNull(call);
+                var frame = new SystemContractExecutionFrame(call!, context, input, 100_000_000);
+                Assert.AreEqual(ExecutionStatus.Ok, contract.GetTotalActiveStake(frame));
+                Assert.AreEqual(totalStake, frame.ReturnValue.ToUInt256().ToMoney());
+            }
+
+            // Get total stake for staker
+            {
+                var input = ContractEncoder.Encode(StakingInterface.MethodGetStakerTotalStake, sender);
+                var call = _contractRegisterer.DecodeContract(context, ContractRegisterer.StakingContract, input);
+                Assert.IsNotNull(call);
+                var frame = new SystemContractExecutionFrame(call!, context, input, 100_000_000);
+                Assert.AreEqual(ExecutionStatus.Ok, contract.GetStakerTotalStake(sender, frame));
+                Assert.AreEqual(totalStake, frame.ReturnValue.ToUInt256().ToMoney());
             }
         }
     }
